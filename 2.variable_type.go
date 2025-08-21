5b3748eac41967776caf25b01c59f1b98fd85c2f
rayhan-bapari <126815340+rayhan-bapari@users.noreply.github.com>
2025-08-22 01:17:36 +0600

    first commit

---
+++ 2.variable_type.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Md.Rayhan Bapari"
+	fmt.Println(name)
+	fmt.Printf("name is of type %T\n", name)
+
+	fmt.Printf("============\n")
+
+	age := 28
+	fmt.Println(age)
+	fmt.Printf("age is of type %T\n", age)
+
+	fmt.Printf("============\n")
+
+	status := false
+	fmt.Println(status)
+	fmt.Printf("status is of type %T\n", status)
+}
